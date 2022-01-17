c0fd0a9545cba1b68d1c62b921e1514b3994befd
Tayyib Mahmood <Tayyibmahmood2001@outlook.com>
2022-01-17 14:07:50 +0000

    controllers

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -3,15 +3,25 @@ const { v4: uuidv4 } = require("uuid");
 
 const { readFromFile, writeToFile } = require("../../util");
 
-const getAppNotes = (req, res) => {
+const getNotes = (req, res) => {
   const filePath = path.join(__dirname, "../../../db/db.json");
   const notes = readFromFile(filePath);
   res.json(notes);
 };
-
- const createNotes = (req, res) => {
-     res.send(`createNotes`);
- };
+const createNotes = (req, res) => {
+    try {
+      const { title, text } = req.body;
+      const id = uuidv4();
+      const newNote = { title, text, id };
+      const filePath = path.join(__dirname, "../../../db/db.json");
+      const notes = readFromFile(filePath);
+      writeToFile(filePath, JSON.stringify([newNote, ...notes]));
+      res.json(newNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ error: "Error!:postAppNotes" });
+    }
+  };
 
   const deleteNotes = (req, res) => {
       res.send(`deletenote`);
